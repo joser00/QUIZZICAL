639e97da385e342e6675dfdab97c25c8153a10f9
Jose R <155120429+joser00@users.noreply.github.com>
2024-03-30 10:39:47 -0400

    erased project

--- src/components/Answer.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default function Answer(props) {
-  return (
-      <span className="all--answer" onClick={props.handleClick}>{props.answer}</span>
-  );
-}
--- src/components/Quizz.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function Quizz (props){
-    return (
-        <div className="question--container">
-            <h1 className="question">{props.question}</h1>
-        </div>
-    )
-}
